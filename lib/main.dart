1577322d44c2071fde2c8423dfd0dc2899ad9d5a
Harshi-W <harshipeiris2001@gmail.com>
2024-02-06 10:05:35 +0530

    Body Type selection added to  main branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const BodySelection());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BodySelection extends StatelessWidget {
+  const BodySelection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(   
+    return MaterialApp(
       home: Scaffold(
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -26,24 +26,39 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top:160.0),
-                  child: CustomButton(imagePath: 'images/type1.jpg', text: 'S',onPressed: (){}),
+                  padding: const EdgeInsets.only(top: 160.0),
+                  child: CustomButton(
+                      imagePath: 'images/type1.jpg',
+                      text: 'S',
+                      onPressed: () {}),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top:110.0),
-                  child: CustomButton(imagePath: 'images/type2.jpg', text: 'M',onPressed: (){}),
+                  padding: const EdgeInsets.only(top: 110.0),
+                  child: CustomButton(
+                      imagePath: 'images/type2.jpg',
+                      text: 'M',
+                      onPressed: () {}),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top:70.0),
-                  child: CustomButton(imagePath: 'images/type3.jpg', text: 'L',onPressed: (){}),
+                  padding: const EdgeInsets.only(top: 70.0),
+                  child: CustomButton(
+                      imagePath: 'images/type3.jpg',
+                      text: 'L',
+                      onPressed: () {}),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top:110.0),
-                  child: CustomButton(imagePath: 'images/type4.jpg', text: 'XL',onPressed: (){}),
+                  padding: const EdgeInsets.only(top: 110.0),
+                  child: CustomButton(
+                      imagePath: 'images/type4.jpg',
+                      text: 'XL',
+                      onPressed: () {}),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top:160.0),
-                  child: CustomButton(imagePath: 'images/type5.jpeg', text: 'XXL+',onPressed: (){}),
+                  padding: const EdgeInsets.only(top: 160.0),
+                  child: CustomButton(
+                      imagePath: 'images/type5.jpeg',
+                      text: 'XXL+',
+                      onPressed: () {}),
                 ),
               ],
             ),
@@ -59,7 +74,11 @@ class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
 
-  const CustomButton({required this.imagePath, required this.text, required this.onPressed, Key? key})
+  const CustomButton(
+      {required this.imagePath,
+      required this.text,
+      required this.onPressed,
+      Key? key})
       : super(key: key);
 
   @override
@@ -75,8 +94,8 @@ class CustomButton extends StatelessWidget {
           ),
           child: Text(
             text,
-          style: const TextStyle(color: Colors.white), 
-        ),
+            style: const TextStyle(color: Colors.white),
+          ),
         ),
       ],
     );
